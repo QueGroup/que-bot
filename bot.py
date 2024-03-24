72f81677191a3bbea0f0020455ca668a4edb4807
David Dzhalaev <72649244+DavidRomanovizc@users.noreply.github.com>
2024-03-24 22:57:55 +0300

    ♻️ minor fixes

--- bot.py
+++ bot.py
@@ -1,89 +1,111 @@
-# noinspection PyUnresolvedReferences
+import asyncio
 import logging
-import os
 
-import django
+import betterlogging as bl
 from aiogram import (
-    executor, Dispatcher,
+    Dispatcher, Bot,
 )
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.storage.redis import RedisStorage, DefaultKeyBuilder
 
-# noinspection PyUnresolvedReferences
-from django_project.telegrambot.telegrambot import (
-    settings,
-)
-from loader import (
-    dp,
-    scheduler,
-)
-from src.tgbot.filters import IsGroup, IsAdmin, IsPrivate
-from src.infrastructure.db_api.db_commands import (
-    reset_view_limit,
-)
-from src.tgbot.services.app.logger import (
-    setup_logger,
-)
-from src.tgbot.services.app.notify_admins import (
-    AdminNotification,
-)
-from src.tgbot.services.app.set_bot_commands import (
-    set_default_commands,
-)
-from src.tgbot.middlewares import (
-    ThrottlingMiddleware,
-    LinkCheckMiddleware,
-    SupportMiddleware,
-    IsMaintenance,
-    SchedulerMiddleware,
-    BanMiddleware,
-    LogMiddleware
+from src.tgbot.config import Config, load_config
+from src.tgbot.handlers import routers_list
+from src.tgbot.middlewares import ConfigMiddleware
+from src.tgbot.services.app import (
+    broadcaster
 )
 
 
-def register_all_middlewares(dp: Dispatcher) -> None:
-    dp.setup_middleware(ThrottlingMiddleware())
-    dp.setup_middleware(LinkCheckMiddleware())
-    dp.setup_middleware(SupportMiddleware())
-    dp.setup_middleware(IsMaintenance())
-    dp.setup_middleware(SchedulerMiddleware(scheduler))
-    dp.setup_middleware(BanMiddleware())
-    dp.setup_middleware(LogMiddleware())
+async def on_startup(bot: Bot, admin_ids: list[int]) -> None:
+    await broadcaster.broadcast(bot, admin_ids, "Бот запущен")
 
 
-def register_all_filters(dp: Dispatcher) -> None:
-    dp.filters_factory.bind(IsGroup)
-    dp.filters_factory.bind(IsPrivate)
-    dp.filters_factory.bind(IsAdmin)
+def register_global_middlewares(
+        dp: Dispatcher,
+        config: Config
+) -> None:
+    middleware_types = [
+        ConfigMiddleware(config)
+        # ThrottlingMiddleware(),
+        # LinkCheckMiddleware(),
+        # SupportMiddleware(),
+        # IsMaintenance(),
+        # SchedulerMiddleware(scheduler),
+        # BanMiddleware(),
+        # LogMiddleware(),
+    ]
+    for middleware_type in middleware_types:
+        dp.message.outer_middleware(middleware_type)
+        dp.callback_query.outer_middleware(middleware_type)
 
 
-async def on_startup(dispatcher) -> None:
-    await set_default_commands(dispatcher)
-    scheduler.add_job(
-        func=reset_view_limit,
-        trigger="cron",
-        hour=0,
-        id="reset_view_limit",
-        replace_existing=True,
+def setup_logging():
+    """
+    Set up logging configuration for the application.
+
+    This method initializes the logging configuration for the application.
+    It sets the log level to INFO and configures a basic colorized log for
+    output. The log format includes the filename, line number, log level,
+    timestamp, logger name, and log message.
+
+    Returns:
+        None
+
+    Example usage:
+        setup_logging()
+    """
+    log_level = logging.INFO
+    bl.basic_colorized_config(level=log_level)
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s",
     )
-    await AdminNotification.send(dispatcher)
+    logger = logging.getLogger(__name__)
+    logger.info("Starting bot")
 
 
-def setup_django():
-    os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE", "django_project.telegrambot.telegrambot.settings"
-    )
-    os.environ.update({"DJANGO_ALLOW_ASYNC_UNSAFE": "true"})
-    django.setup()
+def get_storage(config):
+    """
+    Return storage based on the provided configuration.
+
+    Args:
+        config (Config): The configuration object.
+
+    Returns:
+        Storage: The storage object based on the configuration.
+
+    """
+    if config.tg_bot.use_redis:
+        return RedisStorage.from_url(
+            config.redis.dsn(),
+            key_builder=DefaultKeyBuilder(with_bot_id=True, with_destiny=True),
+        )
+    else:
+        return MemoryStorage()
+
+
+async def main():
+    setup_logging()
+
+    config = load_config()
+    storage = get_storage(config)
+
+    bot = Bot(token=config.tg_bot.token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    dp = Dispatcher(storage=storage)
+
+    dp.include_routers(*routers_list)
+
+    register_global_middlewares(dp, config)
+
+    await on_startup(bot, config.tg_bot.admin_ids)
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    setup_django()
-    setup_logger("INFO", ["aiogram.bot.api"])
-    # noinspection PyUnresolvedReferences
-    from src.tgbot import (
-        filters,
-        middlewares,
-        handlers
-    )
-
-    scheduler.start()
-    executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        logging.error("Бот был выключен!")
