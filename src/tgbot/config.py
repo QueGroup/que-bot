8c528d7ad5029ea3dec5637d43fe6ba88a2f16ee
dromanov <dtdzhalaev@gmail.com>
2024-04-23 18:33:33 +0300

    ♻️ minor fixes

--- src/tgbot/config.py
+++ src/tgbot/config.py
@@ -1,10 +1,10 @@
-import inspect
 from dataclasses import (
     dataclass,
 )
 from functools import (
     lru_cache,
 )
+import inspect
 from pathlib import (
     Path,
 )
@@ -12,49 +12,6 @@ from pathlib import (
 from environs import (
     Env,
 )
-from yarl import (
-    URL,
-)
-
-
-@dataclass(frozen=True, slots=True)
-class DataBaseConfig:
-    """
-    Database configuration class.
-    This class holds the settings for the database, such as host, password, port, etc.
-
-    Attributes
-    ----------
-    host : str
-        The host where the database server is located.
-    password : str
-        The password used to authenticate with the database.
-    user : str
-        The username used to authenticate with the database.
-    database : str
-        The name of the database.
-    port : int
-        The port where the database server is listening.
-    """
-    user: str
-    password: str
-    host: str
-    database: str
-    port: str
-
-    @staticmethod
-    def from_env(env: Env):
-        """
-        Creates the DbConfig object from environment variables.
-        """
-        host = env.str("DB_HOST")
-        password = env.str("POSTGRES_PASSWORD")
-        user = env.str("POSTGRES_USER")
-        database = env.str("POSTGRES_DB")
-        port = env.int("DB_PORT", 5432)
-        return DataBaseConfig(
-            host=host, password=password, user=user, database=database, port=port
-        )
 
 
 @dataclass
@@ -86,7 +43,7 @@ class RedisConfig:
             return f"redis://{self.redis_host}:{self.redis_port}/0"
 
     @staticmethod
-    def from_env(env: Env):
+    def from_env(env: Env) -> "RedisConfig":
         """
         Creates the RedisConfig object from environment variables.
         """
@@ -104,7 +61,6 @@ class TgBot:
     support_ids: list[int]
     timezone: str
     ip: str
-    I18N_DOMAIN: str
     moderate_chat: int
     use_redis: bool
 
@@ -118,7 +74,6 @@ class TgBot:
         support_ids = list(map(int, env.list("SUPPORTS")))
         ip = env.str("IP")
         timezone = env.str("TIMEZONE")
-        I18N_DOMAIN = "dating"
         moderate_chat = env.int("MODERATE_CHAT")
         use_redis = env.bool("USE_REDIS")
         return TgBot(
@@ -127,7 +82,6 @@ class TgBot:
             support_ids=support_ids,
             ip=ip, timezone=timezone,
             moderate_chat=moderate_chat,
-            I18N_DOMAIN=I18N_DOMAIN,
             use_redis=use_redis,
         )
 
@@ -135,37 +89,21 @@ class TgBot:
 @dataclass(frozen=True, slots=True)
 class Miscellaneous:
     secret_key: str
-    yandex_api_key: str
-    client_id: str
-    redirect_url: URL
-    yoomoney_key: str
-    production: bool
 
     @staticmethod
     def from_env(env: Env) -> "Miscellaneous":
         """
         Creates the Miscellaneous object from environment variables.
         """
-        secret_key = env.str("SECRET_KEY")
-        yandex_api_key = env.str("API_KEY")
-        client_id = env.str("CLIENT_ID")
-        redirect_url = env.str("REDIRECT_URI")
-        yoomoney_key = env.str("YOOMONEY_KEY")
-        production = env.bool("PRODUCTION")
+        secret_key = env.str("SIGNATURE_SECRET_KEY")
         return Miscellaneous(
             secret_key=secret_key,
-            yandex_api_key=yandex_api_key,
-            client_id=client_id,
-            redirect_url=redirect_url,
-            production=production,
-            yoomoney_key=yoomoney_key
         )
 
 
 @dataclass(frozen=True, slots=True)
 class Config:
     tg_bot: TgBot
-    db: DataBaseConfig
     misc: Miscellaneous
     redis: RedisConfig | None = None
 
@@ -209,6 +147,5 @@ def load_config() -> Config:
 
     return Config(
         tg_bot=TgBot.from_env(env),
-        db=DataBaseConfig.from_env(env),
         misc=Miscellaneous.from_env(env),
     )
