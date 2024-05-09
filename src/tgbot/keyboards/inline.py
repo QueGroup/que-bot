50dc944624ea11e81f7fa844aad352b339f75370
dromanov <dtdzhalaev@gmail.com>
2024-05-09 23:27:54 +0300

    💄 add profile menu

--- src/tgbot/keyboards/inline.py
+++ src/tgbot/keyboards/inline.py
@@ -4,6 +4,9 @@ from aiogram import (
 from aiogram.types import (
     WebAppInfo,
 )
+from aiogram.utils.i18n import (
+    gettext as _,
+)
 from aiogram.utils.keyboard import (
     InlineKeyboardBuilder,
 )
@@ -20,10 +23,14 @@ def about_project_menu() -> types.InlineKeyboardMarkup:
 def user_menu() -> types.InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
     builder.row(
-        types.InlineKeyboardButton(text="👤 Мой профиль", callback_data="user:profile")
+        types.InlineKeyboardButton(text=_("👤 Мой профиль"), callback_data="user:profile"),
     )
     builder.row(
-        types.InlineKeyboardButton(text="🔙 Выйти из аккаунта", callback_data="user:signout")
+        types.InlineKeyboardButton(text=_("Изменить"), callback_data="user:edit"),
+        types.InlineKeyboardButton(text=_("Устройства"), callback_data="user:session")
+    )
+    builder.row(
+        types.InlineKeyboardButton(text=_("🔙 Выйти из аккаунта"), callback_data="user:signout")
     )
 
     return builder.as_markup()
@@ -32,6 +39,18 @@ def user_menu() -> types.InlineKeyboardMarkup:
 def user_activation_menu() -> types.InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
     builder.row(
-        types.InlineKeyboardButton(text="🚀 Активировать", callback_data="user:activate")
+        types.InlineKeyboardButton(text=_("🚀 Активировать"), callback_data="user:activate")
+    )
+    return builder.as_markup()
+
+
+def profile_menu() -> types.InlineKeyboardMarkup:
+    builder = InlineKeyboardBuilder()
+    builder.row(
+        types.InlineKeyboardButton(text="Изменить", callback_data="profile:edit"),
+        types.InlineKeyboardButton(text="Удалить", callback_data="profile:delete")
+    )
+    builder.row(
+        types.InlineKeyboardButton(text="<< Вернуться назад", callback_data="back_to_user_menu")
     )
     return builder.as_markup()
