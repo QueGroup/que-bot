2c9bbcd62ba8f8991610c3e6e64ecc62767342f3
dromanov <dtdzhalaev@gmail.com>
2024-06-06 22:31:29 +0300

    🚧 add base implementation for profile

--- src/tgbot/handlers/profile.py
+++ src/tgbot/handlers/profile.py
@@ -12,6 +12,9 @@ from aiogram.fsm.context import (
 from aiogram.types import (
     CallbackQuery,
 )
+from que_sdk import (
+    QueClient,
+)
 
 from src.tgbot.filters import (
     ChatTypeFilter,
@@ -28,8 +31,16 @@ profile_router.message.filter(
 
 @profile_router.callback_query(F.data == "user:profile")
 async def profile_handler(call: CallbackQuery, state: FSMContext, **middleware_data: Any) -> None:
-    # que_client: QueClient = middleware_data.get("que-client")
-    # storage = await state.get_data()
+    que_client: QueClient = middleware_data.get("que-client")
+    storage = await state.get_data()
+    _, profile = await que_client.get_profile(user_id=storage.get("id"), access_token=storage.get("access_token"))
     # await call.message.delete()
     # await call.message.answer_photo()
-    await call.message.edit_text(text="Ваш профиль: ", reply_markup=inline.profile_menu())
+    await call.message.edit_text(
+        text="Ваш профиль: {profile_id}".format(profile_id=profile.get("id")), reply_markup=inline.profile_menu()
+    )
+
+
+@profile_router.callback_query(F.data == "user:profile-create")
+async def profile_create_handler(call: CallbackQuery, state: FSMContext, **middleware_data: Any) -> None:
+    await call.message.edit_text("Вы должны заполнить анкету")
