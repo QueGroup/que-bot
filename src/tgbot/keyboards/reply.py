0f80812d88c4a2c8b3eaccccec9512fe021e40eb
dromanov <dtdzhalaev@gmail.com>
2024-06-14 14:26:15 +0300

    💄 add new keyboards

--- src/tgbot/keyboards/reply.py
+++ src/tgbot/keyboards/reply.py
@@ -34,3 +34,47 @@ def login_signup_menu() -> types.ReplyKeyboardMarkup:
         types.KeyboardButton(text=_("🔑 Войти в аккаунт"), web_app=WebAppInfo(url="https://floppy-phones-camp.loca.lt")),
     )
     return builder.as_markup(resize_keyboard=True)
+
+
+def gender_menu() -> types.ReplyKeyboardMarkup:
+    builder = ReplyKeyboardBuilder()
+
+    builder.row(
+        types.KeyboardButton(text="♂ Мужской"),
+        types.KeyboardButton(text="♀ Женский"),
+    )
+
+    return builder.as_markup(resize_keyboard=True)
+
+
+def interested_in_gender_menu() -> types.ReplyKeyboardMarkup:
+    builder = ReplyKeyboardBuilder()
+
+    builder.row(
+        types.KeyboardButton(text="♂ Парня"),
+        types.KeyboardButton(text="♀ Девушку"),
+    )
+
+    return builder.as_markup(resize_keyboard=True)
+
+
+def hobbies_menu() -> types.ReplyKeyboardMarkup:
+    hobbies = [
+        ("Спорт", "sports"),
+        ("Музыка", "music"),
+        ("Путешествия", "travelling"),
+        ("Готовка", "cooking"),
+        ("Компьютерные игры", "gaming")
+    ]
+    builder = ReplyKeyboardBuilder()
+
+    for hobby_name, hobby_callback in hobbies:
+        button_text = f"{hobby_name}"
+        builder.row(
+            types.KeyboardButton(text=button_text)
+        )
+        builder.adjust(1, 2)
+    builder.row(
+        types.KeyboardButton(text="Подтвердить выбор"),
+    )
+    return builder.as_markup(resize_keyboard=True)
