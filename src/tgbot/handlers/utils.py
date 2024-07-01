5759a4e22babb6380fb9a6ff76768cec7f728729
dromanov <dtdzhalaev@gmail.com>
2024-07-01 15:20:18 +0300

    🔥 remove codes

--- src/tgbot/handlers/utils.py
+++
@@ -1,6 +0,0 @@
-import os
-
-# TODO: Вынести в misc
-
-def path_join(folder_path: str, file_name: str) -> str:
-    return os.path.join(folder_path, file_name)
--- src/tgbot/misc/const.py
+++
@@ -1,9 +0,0 @@
-genders = {
-    "♂ Мужской": "male",
-    "♀ Женский": "female"
-}
-
-interested_genders = {
-    "male": "Парня",
-    "female": "Девушку",
-}
